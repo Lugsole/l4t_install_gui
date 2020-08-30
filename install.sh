76a6d44109411884192052fab27101b0b3dc3279
Lugsole <Lugsole@users.noreply.github.com>
2020-08-30 11:54:52 -0400

    Fixed a thing or two try again

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ echo "moving libhandy source"
 cd libhandy-master
 echo last sucesfull commit test was: d22356ef5f0cc6c6080896ae5611677d59849f51
 echo "installing libhandy dependencies"
-sudo apt install -y gtk-doc-tools libgirepository1.0-dev libgladeui-dev libglib2.0-doc libgnome-desktop-3-dev libgtk-3-doc libgtk-3-dev libxml2-utils meson pkg-config valac cmake gtk+-3.0-dev
-
+sudo apt install -y gtk-doc-tools libgirepository1.0-dev libgladeui-dev libglib2.0-doc libgnome-desktop-3-dev libgtk-3-doc libgtk-3-dev libxml2-utils pkg-config valac cmake gtk+-3.0-dev
+echo "installing l4t_install_gui pip dependencies 2"
+pip3 install meson
 echo "installing changing libhandy dependencies"
 sed 's/3.24.1/3.22.30/g' -i src/meson.build
 
@@ -28,11 +29,9 @@ unzip l4t_install_gui-master.zip
 echo "moving l4t_install_gui source"
 cd l4t_install_gui-master
 echo "installing l4t_install_gui dependencies"
-sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 python3-pip ninja-build libglib1.0-dev
-echo "installing l4t_install_gui pip dependencies 1"
+sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 python3-pip ninja-build libglib2.0-dev
+echo "installing l4t_install_glibgui pip dependencies 1"
 pip3 install pyyaml
-echo "installing l4t_install_gui pip dependencies 2"
-pip3 install meson
 
 echo "configuring l4t_install_gui"
 meson --prefix /usr/local . _build
