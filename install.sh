43f710ac9fe6281347e95bb12632ccea03399ab8
Lugsole <Lugsole@users.noreply.github.com>
2020-08-30 11:28:35 -0400

    fixed instalation of libhandy dependencies

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "moving libhandy source"
 cd libhandy-master
 echo last sucesfull commit test was: d22356ef5f0cc6c6080896ae5611677d59849f51
 echo "installing libhandy dependencies"
-sudo apt get install -y gtk-doc-tools libgirepository1.0-dev libgladeui-dev libglib2.0-doc libgnome-desktop-3-dev libgtk-3-doc libgtk-3-dev libxml2-utils meson pkg-config valac cmake gtk+-3.0-dev
+sudo apt install -y gtk-doc-tools libgirepository1.0-dev libgladeui-dev libglib2.0-doc libgnome-desktop-3-dev libgtk-3-doc libgtk-3-dev libxml2-utils meson pkg-config valac cmake gtk+-3.0-dev
 
 echo "installing changing libhandy dependencies"
 sed 's/3.24.1/3.22.30/g' -i src/meson.build
